a1eb292517cc7cdc366decbf8363a2a21d1457f8
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2025-10-01 12:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (80.35%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int depth = 0;
+        for (char c : s) {
+            if (c == '(') {
+                if (depth++) ans +=c;
+            } else {
+                if (--depth) ans +=c;
+            }
+        }
+        return ans;
+    }
+};

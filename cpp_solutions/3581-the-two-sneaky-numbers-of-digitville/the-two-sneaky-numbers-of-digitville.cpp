54ff86a7e07bc078483cf8d35d1d438b997bc32a
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2025-10-31 11:45:54 +0530

    Time: 3 ms (40.61%) | Memory: 26.3 MB (47.53%) - LeetSync

---
+++ cpp_solutions/3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        unordered_set<int> seen;
+        vector<int> ans;
+        for (int x : nums) {
+            if (seen.count(x))
+                ans.push_back(x);
+            else
+                seen.insert(x);
+        }
+        return ans;
+    }
+};

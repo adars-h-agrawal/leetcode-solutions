632784a2bad8128ee670cffb2d0adc023e5c2c9f
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2025-11-14 10:19:54 +0530

    Time: 14 ms (85.89%) | Memory: 93.4 MB (21.94%) - LeetSync

---
+++ cpp_solutions/2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> diff(n + 1, vector<int>(n + 1, 0));
+
+        // Apply each query to the 2D difference array
+        for (auto &q : queries) {
+            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
+
+            diff[r1][c1] += 1;
+            if (c2 + 1 < n) diff[r1][c2 + 1] -= 1;
+            if (r2 + 1 < n) diff[r2 + 1][c1] -= 1;
+            if (r2 + 1 < n && c2 + 1 < n) diff[r2 + 1][c2 + 1] += 1;
+        }
+
+        // Convert the diff array into the final result matrix using prefix sums
+        vector<vector<int>> res(n, vector<int>(n, 0));
+        
+        // Row-wise prefix sum
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                diff[i][j] += diff[i][j - 1];
+            }
+        }
+
+        // Column-wise prefix sum
+        for (int j = 0; j < n; j++) {
+            for (int i = 1; i < n; i++) {
+                diff[i][j] += diff[i - 1][j];
+            }
+        }
+
+        // Fill result
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = diff[i][j];
+            }
+        }
+
+        return res;
+    }
+};

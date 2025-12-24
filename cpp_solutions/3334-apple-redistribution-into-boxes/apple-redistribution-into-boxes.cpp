5f3214f3784187d8d09fe50e7ae1c858e32c4019
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2025-12-24 12:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (45.84%) - LeetSync

---
+++ cpp_solutions/3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int totalApples = 0;
+        for (int a : apple) totalApples += a;
+
+        sort(capacity.begin(), capacity.end(), greater<int>());
+
+        int usedCapacity = 0;
+        int boxes = 0;
+
+        for (int c : capacity) {
+            usedCapacity += c;
+            boxes++;
+            if (usedCapacity >= totalApples) {
+                return boxes;
+            }
+        }
+        return boxes;
+    }
+};

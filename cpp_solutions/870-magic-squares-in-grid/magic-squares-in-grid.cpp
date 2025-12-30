074da1fbafdcb1abfc98452316ea2f7997083001
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2025-12-30 13:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (72.04%) - LeetSync

---
+++ cpp_solutions/870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isMagic(vector<vector<int>>& g, int r, int c) {
+        // center must be 5
+        if (g[r+1][c+1] != 5) return false;
+
+        vector<int> cnt(10, 0);
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                int v = g[i][j];
+                if (v < 1 || v > 9 || cnt[v]++) return false;
+            }
+        }
+
+        int s = g[r][c] + g[r][c+1] + g[r][c+2];
+
+        for (int i = 0; i < 3; i++) {
+            if (g[r+i][c] + g[r+i][c+1] + g[r+i][c+2] != s) return false;
+            if (g[r][c+i] + g[r+1][c+i] + g[r+2][c+i] != s) return false;
+        }
+
+        if (g[r][c] + g[r+1][c+1] + g[r+2][c+2] != s) return false;
+        if (g[r][c+2] + g[r+1][c+1] + g[r+2][c] != s) return false;
+
+        return true;
+    }
+
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0;
+
+        for (int i = 0; i + 2 < n; i++) {
+            for (int j = 0; j + 2 < m; j++) {
+                if (isMagic(grid, i, j)) ans++;
+            }
+        }
+        return ans;
+    }
+};

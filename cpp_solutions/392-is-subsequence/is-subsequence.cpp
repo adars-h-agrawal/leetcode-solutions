41748c45d882a9a81ba82f78253e0d7cf7d4dca8
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2026-01-20 18:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (89.87%) - LeetSync

---
+++ cpp_solutions/392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s == "") return true;
+        if (t == "") return false;
+        int i = 0, j = 0, m = s.size(), n = t.size();
+        while (j < n) {
+            if (s[i] == t[j]) {
+                i++; 
+            }
+            j++;
+        }
+        return i >= m;
+    }
+};

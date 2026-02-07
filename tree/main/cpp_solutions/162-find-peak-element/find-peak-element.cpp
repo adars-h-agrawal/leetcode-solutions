afb68e8b9305f62137df82b15e6dd20fcc88873f
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2026-02-07 15:53:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (25.93%) - LeetSync

---
+++ tree/main/cpp_solutions/162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size(), left = 0, right = n-1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid+1]) right = mid;
+            else left = mid+1;
+        }
+        return left; // or right
+    }
+};

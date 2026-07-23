82e7b2edd7ba54380f844e4ddb56b95a61c076cf
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2026-07-23 14:50:10 +0530

    Time: 71 ms (34.79%) | Memory: 111.3 MB (20.84%) - LeetSync

---
+++ cpp_solutions/217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int n : nums) {
+            if (freq[n] == 1)
+                return true;
+            freq[n]++;
+        }
+        return false;
+    }
+};

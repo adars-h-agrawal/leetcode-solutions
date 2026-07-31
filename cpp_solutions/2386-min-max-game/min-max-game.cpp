43fc803af20e68b730aaf3400e9c62824a2a7b19
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2026-07-31 15:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (61.13%) - LeetSync

--- cpp_solutions/2386-min-max-game/min-max-game.cpp
+++ cpp_solutions/2386-min-max-game/min-max-game.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int minMaxGame(vector<int>& nums) {
-        vector<int> tmp = nums;
         int n = nums.size();
+        
         while (n > 1) {
-            vector<int> cur(n/2);
             for (int i = 0; i < n/2; i++) {
                 if (i % 2 == 0) {
-                    cur[i] = min(tmp[2*i], tmp[2*i+1]);
+                    nums[i] = min(nums[2*i], nums[2*i+1]);
                 } else {
-                    cur[i] = max(tmp[2*i], tmp[2*i+1]);
+                    nums[i] = max(nums[2*i], nums[2*i+1]);
                 }
             }
-            tmp = cur;
             n /= 2;
         }
-        return tmp[0];
+        return nums[0];
     }
 };

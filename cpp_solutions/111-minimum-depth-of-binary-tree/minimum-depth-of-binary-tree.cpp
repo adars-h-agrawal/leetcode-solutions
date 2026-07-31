15140a5bca81dc8b34ff836a6e4af9fcc568b4f5
Adarsh Agrawal <148339708+adars-h-agrawal@users.noreply.github.com>
2026-07-31 15:40:31 +0530

    Time: 3 ms (54.24%) | Memory: 146.8 MB (78.81%) - LeetSync

--- cpp_solutions/111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ cpp_solutions/111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -11,20 +11,10 @@
  */
 class Solution {
 public:
-    int ans = 1e5;
-    void dfs(TreeNode* node, int depth) {
-        if (!node) return;
-        if (!node->left && !node->right) {
-            ans = min(ans, depth);
-            return;
-        }
-        dfs(node->left, depth + 1);
-        dfs(node->right, depth + 1);
-    }
-
     int minDepth(TreeNode* root) {
         if (!root) return 0;
-        dfs(root, 1);
-        return ans;
+        if (!root->left) return 1 + minDepth(root->right);
+        if (!root->right) return 1 + minDepth(root->left);
+        return 1 + min(minDepth(root->left), minDepth(root->right));
     }
 };
